af6ec5ed1521cf831dc47370421166b38aa85aa1
mdshahed89 <lifetime8954@gmail.com>
2024-01-14 03:01:10 +0600

    first route and controller

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { Express } from "express";
+import Express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 
-const app = express();
+const app = Express();
 
 app.use(cors({
   origin : process.env.CORS_ORIGIN,
   credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(Express.json({limit: "16kb"}))
+app.use(Express.urlencoded({extended: true, limit: "16kb"}))
+app.use(Express.static("public"))
 app.use(cookieParser())
+
+
+
+
+// router imports
+import userRouter from "./routes/user.router.js"
+
+
+// router decleration
+app.use("/api/v1/users", userRouter)
 
 
 export default app
